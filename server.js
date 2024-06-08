41413006fe7cc68220e18bdb4a1cd3359dd280bd
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-06-09 02:42:28 +0600

    DB connection done

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+const { connect } = require("./database/dbconfig");
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const router = require("./routes");
 
+//DB connection
+connect();
+
+//Express and Middleware
 const app = express();
 app.use(cors());
 app.use(router);
 
 const Port = process.env.PORT || 8000;
 
-app.listen(Port, () => {
-  console.log("Express");
-});
+app.listen(Port);
